36253b399a0da7354920f00e853dccb399f5e91f
Rohithv17 <rohithv1396@gmail.com>
2024-02-09 11:23:40 +0530

    Added functionality to return sad mood as the output

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import com.bridgelabz.MoodAnalyzer.PersonMoodAnalyzer;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
+        PersonMoodAnalyzer p = new PersonMoodAnalyzer("I am in sad mood"); //Giving this message to get the output "SAD"
+        System.out.println("Mood of the person is : "+p.analyzeMood());
 
         }
     }
--- src/com/bridgelabz/MoodAnalyzer/PersonMoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer/PersonMoodAnalyzer.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.MoodAnalyzer;
 
 public class PersonMoodAnalyzer {
+
+    private final String msg;
+
+    public PersonMoodAnalyzer(String msg){
+        this.msg=msg;
+
+    }
+
+    public String analyzeMood(){  // Method to find mood of the person
+
+        if(msg.contains("sad")){
+            return "SAD";
+        }
+        else {
+            return "Invalid Mood";
+        }
+
+    }
 }
